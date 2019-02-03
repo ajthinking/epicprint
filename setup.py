4b627feb665ca117c53d5fc0cde0547b838e28fd
queensferry <queensferry.me@gmail.com>
2019-02-03 12:57:10 +0800

    fix: hanging indent conforming to PEP8

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='epicprint',  
-    version='0.4',
-    author="Anders Jürisoo",
-    author_email="jurisoo@hotmail.com",
-    description="Custom print with superpowers",
-    long_description=long_description,
-    url="https://github.com/ajthinking/epicprint",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+     name='epicprint',  
+     version='0.4',
+     author="Anders Jürisoo",
+     author_email="jurisoo@hotmail.com",
+     description="Custom print with superpowers",
+     long_description=long_description,
+     url="https://github.com/ajthinking/epicprint",
+     packages=find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
